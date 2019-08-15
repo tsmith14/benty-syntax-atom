2c288a9518311327f8652e0586fbfb052303b67d
Tyler Smith <tyler@benty.io>
2019-08-15 16:02:01 -0400

    Update to fix Atom syntax issues

---
+++ samples/javascript.js
@@ -0,0 +1,26 @@
+import Square from './square';
+
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
+    set width(width) {
+        this._width = width;
+    }
+    get width() {
+        return this._width;
+    }
+    set height(height) {
+        this._height = height;
+    }
+    get height() {
+        return this._height;
+    }
+    get area() {
+        return this._width * this._height;
+    }
+};
+
+var r = new Rectangle(50, 20);
+console.log(`Compare rectangle.area === 1000:`, r.area === 1000);
